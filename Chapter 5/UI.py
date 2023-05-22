a0ae5a7cabb8c91f0941980b024168c709ed7679
Prethve Deev <diamondspree431@gmail.com>
2023-05-22 10:47:37 +0800

    Building Callbacks with Multiple Inputs and Outputs (2 Input, 1 Output)

--- Chapter 5/UI.py
+++ Chapter 5/UI.py
@@ -17,19 +17,26 @@ app.layout = html.Div([
             dcc.Dropdown(options=(happiness['country']).unique(), 
                          value='United States', 
                          id='country_input'),
+            dcc.RadioItems(id='radio_options',
+                           options={
+                               'happiness_rank':'Happiness Rank',
+                               'happiness_score':'Happiness Score'
+                           },
+                           value='happiness_rank'),
             dcc.Graph(id='happiness_graph')
 ])
 
 @app.callback(
     Output(component_id='happiness_graph', component_property='figure'),
     Input(component_id='country_input',component_property='value'),
+    Input(component_id='radio_options', component_property='value')
 )
-def update_graph(selected_country):
+def update_graph(selected_country, selected_option):
     filtered_happiness = happiness[happiness['country'] == selected_country]
     line_fig = px.line(filtered_happiness,
                        x='year',
-                       y='happiness_score',
-                       title=f'Happiness Score in {selected_country}')
+                       y=selected_option,
+                       title=f'{selected_option} in {selected_country}')
     return line_fig
 
 if __name__ == '__main__':
